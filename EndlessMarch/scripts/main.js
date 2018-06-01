fd63fcb3c1f3d85950494cb79e2cd5ddde045505
AbowlofStew <saxup@ucsc.edu>
2018-06-01 12:31:49 -0700

    partial of naritive framwork

--- EndlessMarch/scripts/main.js
+++ EndlessMarch/scripts/main.js
@@ -7,6 +7,7 @@ var population =1000;
 var food = 1000;
 var supplies=1000;
 var currentBGM;
+var cne;//curent narative event
 //\\_____________________//\\
 //\\ Global variables    //\\
 //\\_____________________//\\
@@ -41,7 +42,7 @@ Preload.prototype = {
     game.load.image('TextWindow','assets/imgs/TextWindow.png')
 
 
-    game.load.atlas('RndButton', 'assets/imgs/RndButton.png','assets/imgs/RndButton.json',
+    	game.load.atlas('RndButton', 'assets/imgs/RndButton.png','assets/imgs/RndButton.json',
 		Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
 
 		game.load.atlas('instructions', 'assets/imgs/instructions.png','assets/imgs/instructions.json'),
@@ -57,173 +58,30 @@ Preload.prototype = {
 
 	},
 	create: function() {
-    var info = {timer: 7, scene: "mainMenu", keepPreload: true, keepCreate: false}
-
-    LoadScene(info);
+    LoadScene('mainMenu');
 
 	}
 }
 
 var Load = function(game) {};
 Load.prototype = {
-  init: function(loadInfo) {
-    this.loadInfo = loadInfo
-
-  },
 	preload: function() {
-    // preload assets
-  	game.load.atlas('loading','assets/imgs/loadingAtlas.png','assets/imgs/loading.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
-  	game.load.image('window','assets/imgs/TextWindow.png');//Replace with UI atlas when its made available
-  	treesSprite = ["Tree1","Tree2","Tree3","Tree4","Tree5","Tree6","skull","Skeleton"];
-  	dustSprite = ["dust1","dust2","dust3"];
-  	food=75;
-  	resources=75;
-  	population=1250;
-  	//replace with global variables
-  	startingFood=100;
-  	startingResources=100;
-  	startingPopulation=1500;
-  	//replace with global variables
-  	//if variable names change check the bottom of create at ~line 90
 	},
 	create: function() {
-    game.add.sprite(0,0,'loading','Gradient');
-  	sun = game.add.sprite(800,535,'loading','Sun');
-  	sun.anchor.setTo(.5,.5);
-  	game.add.sprite(0,443,'loading','Mountains');
-  	hills = game.add.tileSprite(0,590,1800,166,'loading','Hills');
-  	game.add.sprite(0,672,'loading','Floor');
 
-  	bgtrees = game.add.group();
-
-  	bgTreeTimer = game.time.create(false);
-  	bgTreeTimer.loop(10000+(Math.random()*1000), createbgTree, this);
-  	bgTreeTimer.start();
-
-  	banner1 = game.add.sprite(346,605,'loading','Flag-1');
-  	banner1.animations.add('flag',[1,2,3,4],4,true);
-  	banner1.animations.play('flag');
-  	banner1.scale.setTo(.5,.5);
-  	banner2 = game.add.sprite(497,602,'loading','Flag-1');
-  	banner2.animations.add('flag',[1,2,3,4],4,true);
-  	banner2.animations.play('flag');
-  	banner2.scale.setTo(.5,.5);
-  	banner3 = game.add.sprite(654,610,'loading','Flag-1');
-  	banner3.animations.add('flag',[1,2,3,4],4,true);
-  	banner3.animations.play('flag');
-  	banner3.scale.setTo(.5,.5);
-  	banner4 = game.add.sprite(877,600,'loading','Flag-1');
-  	banner4.animations.add('flag',[1,2,3,4],4,true);
-  	banner4.animations.play('flag');
-  	banner4.scale.setTo(.5,.5);
-
-  	army = game.add.sprite(210,660,'loading','Army-1');
-  	armyStretch = 0.
-  	armyDir = 0;
-
-  	heads1 = game.add.sprite(260,665,'loading','Heads1');
-  	headBobDir1 = 0;
-  	heads2 = game.add.sprite(260,665,'loading','Heads2');
-  	headBobDir2 = 0;
-
-  	fgtrees = game.add.group();
-
-  	fgTreeTimer = game.time.create(false);
-  	fgTreeTimer.loop(4000+(Math.random()*5000), createfgTree, this);
-  	fgTreeTimer.start();
-
-  	armyTimer = game.time.create(false);
-  	armyTimer.loop(3000, switchDir, this);
-  	headBob1Timer1 = game.time.create(false);
-  	headBob1Timer1.loop(1500, HeadDir1, this);
-  	headBob1Timer1.start();
-  	headBob1Timer2 = game.time.create(false);
-  	headBob1Timer2.loop(1500, HeadDir2, this);
-  	headBob1Timer2.start();
-
-  	dust = game.add.emitter(510, 665, 100)
-  	dust.makeParticles('loading',dustSprite[Math.round(Math.random()*3)]);
-  	dust.minParticleAlpha = 0.01;
-  	dust.maxParticleAlpha = 0.1;
-  	dust.minParticleScale = .5;
-  	dust.maxParticleScale = 1;
-  	dust.setXSpeed(-70,-55);
-  	dust.setYSpeed(-50,0);
-  	dust.gravity = 0;
-  	dust.maxRotation = 0;
-  	dust.flow(10000,500,5,-1);
-
-  	Lwindow = game.add.sprite(225,30,'window');
-  	Lwindow.scale.setTo(.4,.4)
-  	Lwindow.alpha = 0;
-  	game.add.tween(Lwindow).to( { alpha: .8 }, 5000, "Linear", true);
-
-  	currentPopulation = game.add.text(285, 170, startingPopulation-population+" People died on this stretch of you journey");
-  	currentFood = game.add.text(285, 220, startingFood-food+" Food was consumed or lost");
-  	currentResources = game.add.text(285, 270, startingResources-resources+" resources were consumer or lost");
-    console.log(this.loadInfo);
-    game.time.events.add(Phaser.Timer.SECOND * this.loadInfo.timer, LoadScene, this, this.loadInfo);
 	},
 	update: function() {
-    hills.tilePosition.x-=.1;
-    sun.angle+=.1;
-    bgtrees.forEachAlive(function(bgtree) {bgtree.x-=.3},this);
-    fgtrees.forEachAlive(function(fgtree) {fgtree.x-=.7},this);
-    if(armyDir == 0){armyStretch+=.0001};
-    if(armyDir == 1){armyStretch-=.0001};
-    army.scale.setTo(1+armyStretch,1-armyStretch);
-    if(headBobDir1==0){heads1.y+=.025};
-    if(headBobDir1==1){heads1.y-=.025};
-    if(headBobDir2==0){heads1.y+=.025};
-    if(headBobDir2==1){heads1.y-=.025};
-	   armyTimer.start();
-
-	    headBob1Timer1 = game.time.create(false);
+		//game.state.start('GamePlay', true, false);
 	}
 }
 
-
-function createbgTree() {
-	var bgtree = bgtrees.create(1300,705+(Math.random()*15),'loading',treesSprite[Math.round(Math.random()*7)])
-	bgtree.scale.setTo(.2+(Math.random()*.2),.2+(Math.random()*.2));
-	bgtree.anchor.setTo(0,1);
-}
-function createfgTree() {
-	var fgtree = fgtrees.create(1300,835-(Math.random()*60),'loading',treesSprite[Math.round(Math.random()*7)])
-	fgtree.scale.setTo(.6+(Math.random()*.3),.6+(Math.random()*.3));
-	fgtree.anchor.setTo(0,1);
-}
-function switchDir(){
-  if(armyDir==0){armyDir=1}else{armyDir=0}
-}
-function HeadDir1(){
-  if(headBobDir1==0){headBobDir1=1}else{headBobDir1=0}
-}
-function HeadDir2(){
-  if(headBobDir2==0){headBobDir2=1}else{headBobDir2=0}
-}
-
-function LoadScene(info) {
-  if(info.combat != null)
-  {
-    game.state.start(info.scene, info.keepPreload, info.keepCreate,info.combat);
-
-  }
-  else {
-    game.state.start(info.scene, info.keepPreload, info.keepCreate);
-  }
-}
-
-function LoadFireScene(info) {
-  game.state.start(info.scene, info.keepPreload, info.keepCreate,info.combat);
+function LoadScene(scene) {
+	game.state.start(scene, true, false);
 }
 
 //Temp Laod Functions
 function LoadCombat() {
-  var combat = {map:"TestMap3", enemies: 5}
-  var info = {timer: 1, scene: "combat", keepPreload: true, keepCreate: false, combat:combat}
-  game.state.start("load", true, false, info);
-	//game.state.start("combat", true, false,'TestMap3');
+	game.state.start("combat", true, false,'TestMap3');
 }
 
 function Narrative() {
--- EndlessMarch/scripts/states/Acts.js
+++ EndlessMarch/scripts/states/Acts.js
@@ -61,8 +61,10 @@ Act1.prototype = {
 		b3t.anchor.set(0.5);
 		var style = { font: "16px Arial", fill: "#000000", wordWrap: true,  };
     console.log(windoww.position);
+    // console.log(windoww.position);
 
     SpawnOldButtons();
+    // SpawnOldButtons();
 
     if(Path.length == 0)
     {
@@ -79,6 +81,21 @@ Act1.prototype = {
         y:600
       };
     }
+    // if(Path.length == 0)
+    // {
+       caravanStart = game.add.button(game.camera.width *0.15, game.camera.height *0.7, 'RndButton', moveWindow, this, 'Hover','Up','Down');
+       caravanStart.anchor.set(0.5);
+    //   //caravanStart.textWindow = "[KEY IN ATLAS]"
+    //   caravanStart.textWindow = ""
+    //   caravanStart.choice1 = "";
+    //   caravanStart.choice2 = "";
+
+    //   Path[cnt++] =
+    //   {
+    //     x:game.camera.width *0.15,
+    //     y:600
+    //   };
+    // }
     //texbox buttons
     //game.add.button(portrait.width * 0.9 ,game.height - portrait.height * 0.4, [Atlas], [Function], this, [Hover], [UP],[Down]);
 		button1 = game.add.button(2000,0, 'RndButton',LoadCombat,this,'Hover','Up','Down');
@@ -86,6 +103,9 @@ Act1.prototype = {
 		button3 = game.add.button(2000,0,'RndButton',holdF,this,'Hover','Up','Down');
 		hold = game.add.button(2000,0, 'narrativeButtons',LoadCombat,this,'hold','hold','hold');
 		descend = game.add.button(2000,0,'narrativeButtons',CreateNewNode,this,'descend','descend','descend');
+		button1 = game.add.button(2000,0, 'RndButton',,this,'Hover','Up','Down');
+		button2 = game.add.button(2000,0,'RndButton',null,this,'Hover','Up','Down');
+		button3 = game.add.button(2000,0,'RndButton',null,this,'Hover','Up','Down');
 		poptxt = game.add.text(16, 16, 'Population: 1000', { fontSize: '32px', fill: '#999999' });
 		supplytxt = game.add.text(300, 16, 'Supplies: 1000', { fontSize: '32px', fill: '#818181' });
 		foodtxt = game.add.text(750, 16, 'Food: 1000', { fontSize: '32px', fill: '#000' });
@@ -125,6 +145,7 @@ var Act2 = function(game) {};
 Act2.prototype = {
   init: function() {
   },
+
 	preload: function() {
 	},
 	create: function() {
@@ -178,21 +199,141 @@ function SpawnOldButtons() {
 function moveWindow(){
 	windoww.x = 560;
 	windoww.y = 400;
+	windoww.x = 60;
+	windoww.y = 200;
 	population -= 70;
 }
 function holdF(){
 	windoww.x = 560;
 	windoww.y = 400;
+function moveWindowOnScreen(){
+	windoww.x = 60;
+	windoww.y = 200;
+}
+function moveWindowOffScreen(){
+	windoww.x = 60;
+	windoww.y = 2000;
+}
+//event functions
+function elderbattleEnd(){
+	b1t="Asscend Mountains";
+	b2t="villagerbattle";
+	button1.callback=rockslide;
+	button2.callback=villagerbattle;
+	moveWindowOnScreen();
+}
+function rockslide(){
+	supplies-=getRandomInt(5);
+	foor-=getRandomInt(5)+3;
+	bt1="Take higher path";
+	bt2="clear path";
+	button1.callback=OrcBattle;
+	button2.callback=clearPath;
+	moveWindowOnScreen();
+}
+function Orcbattle(){
+	b1t="Start Comabt";
+	button1.callback=LoadCombat;
+	cne=weatherDecision;
+	moveWindowOnScreen();
+}
+function weatherDecision(){
+	supplies-=getRandomInt(5);
+	foor-=getRandomInt(5)+3;
+	bt1="Risk Storm";
+	bt2="Decend to the coast";
+	button1.callback=RiskStorm;
+	button2.callback=LoadCombat;//banditlevel
+	moveWindowOnScreen();
+}
+function RiskStorm(){
+	supplies-=getRandomInt(5)+3;
+	food-=getRandomInt(12);
+	if(getRandomInt(1)==1){
+		population-=getRandomInt(100)+12;
+		supplies-=getRandomInt(12)+12;
+		food-=getRandomInt(12);
+	}
+	//texthere;
+	bt1="Mountain Decent";
+	button1.callback=mountainDesent;
 
 }
-function descendF(){
-	windoww.x = 560;
-	windoww.y = 400;
-	supplies -= 150;
+function mountainDesent(){
+	bt1="go fast";
+	bt2="go slow";
+	button1.callback=gofaast
+}
+function foothilbandits(){
+	bt1="take higherpath"
+	bt2="decend mountains"
+	button1.callback=RiskStorm;
+	button2.callback=crossroad;
+}
+function villagerBattle(){
+	//settext
+	b1t="Start Comabt";
+	button1.callback=LoadCombat;
+	//end of this battle nbeeds to have cne set to either vblost or vbwon
 }
 function elderbattle(){
 
+function vbwon(){
+	b1t="Lootvilage";
+	b2t="recruit villagers";
+	button1.callback=lootvillage;
+	button2.callback=recruitvillagers;
 }
+function vblost(){
+	bt1="Move on";
+	bt2="avenge";
+	button1.callback=rest;
+	button2.callback=avenge;
+}
+function lootvillage(){
+	supplies+=getRandomInt(10);
+	bt1="rest";
+	//textimage call
+
+}
+function recruitvillagers(){
+	population+=getRandomInt(10);
+	bt1="rest";
+	button1=rest();
+
+}
+function rest(){
+
+	bt1="Take rest";
+	bt2="press on";
+	//text image call
+	button1.callback=takeRest;
+	button2.callback= PressOn;
+}
+
+function takeRest(){
+	//rest image call
+	bt1="mountain pass"
+	bt2="foothill Bandits"
+	button1.callback=crossroad;
+	button2.callback=foothilbandits;
+}
+function crossroad(){
+	bt1="raider batle";
+	bt2="avoid village";
+	button1.callback=raiderBattle;//loadcomabt;
+	button2.callback=avoidVillage;
+}
+function avoidVillage(){
+	bt1="border battle";
+}
+function 
+//apropriated from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
 
 
 function DrawPath(){
